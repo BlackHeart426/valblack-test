fda8b5565d131e5128bd46bcda01da59eed30ea1
BlackHeart <valepa007@gmail.com>
2020-09-04 17:16:52 +0300

    add calculation of indicators

--- client/src/services/resultTestService.ts
+++ client/src/services/resultTestService.ts
@@ -10,17 +10,58 @@ interface IAnswersCurrentTest {
     userTestID: string
 }
 
+interface IAnswer {
+    name: string,
+    _id: string,
+    current: boolean,
+    userAnswer: boolean
+}
 // additionAnswerAtTemplateTest
 
 export function resultTestService(templateTest: any , answersCurrentTest: IAnswersCurrentTest, uuidUser: string) {
+    const templateWithAnswer = additionAnswerAtTemplateTest(templateTest, answersCurrentTest)
+    const count = countTheCorrectAnswersAtTemplate(templateWithAnswer)
+    const testPassed = checkResultTest(count.correctQuestion, count.summaryAnswer)
     const resultTest = {
         _id: answersCurrentTest.userTestID,
         userId: uuidUser,
+        rightAnswer: count.correctQuestion,
+        summaryAnswer: count.summaryAnswer,
+        testPassed: testPassed,
         testId: answersCurrentTest.testId,
-        templateWithAnswer: JSON.stringify(additionAnswerAtTemplateTest(templateTest, answersCurrentTest))
+        templateWithAnswer: JSON.stringify(templateWithAnswer)
     }
     console.log(resultTest)
     return resultTest
+}
+
+function checkResultTest(correctQuestion: number, summaryAnswer: number) {
+    console.log(correctQuestion)
+    console.log(summaryAnswer)
+    const resultTestRate = (correctQuestion/summaryAnswer) * 100
+    return resultTestRate > 75 ? true : false
+}
+
+function countTheCorrectAnswersAtTemplate(templateWithAnswer: any) {
+    let correctQuestion = 0
+    let summaryAnswer = 0
+    Object.values(templateWithAnswer).forEach((question: any) => {
+        summaryAnswer++
+        let checkAnswers = false
+        let throwFlag = false
+        Object.values(question.answers).forEach((answer: any) => {
+            if (answer.current === true) {
+                if (answer.userAnswer === true) {
+                    checkAnswers = true
+                } else {
+                    checkAnswers = false
+                    throwFlag = true
+                }
+            }
+        } )
+        !throwFlag && checkAnswers && ++correctQuestion
+    })
+    return {correctQuestion, summaryAnswer}
 }
 
 function additionAnswerAtTemplateTest(templateTest: any , answersCurrentTest: IAnswersCurrentTest) {
