bd36366cc4db2675444adc3eedbcf2df7b8893f1
BlackHeart <valepa007@gmail.com>
2020-09-13 12:38:20 +0300

    fix activity

--- client/src/pages/profile/subPage/activity/cardActivity/CardActivity.tsx
+++ client/src/pages/profile/subPage/activity/cardActivity/CardActivity.tsx
@@ -2,9 +2,10 @@ import {styleCardActivity} from "./styleCardActivity";
 import React from "react";
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import Grid from "@material-ui/core/Grid";
-import {green} from "@material-ui/core/colors";
+import {green, red} from "@material-ui/core/colors";
 import {Paper, Typography} from "@material-ui/core";
-import {useHistory} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
+import moment from "moment";
 
 export const CardActivity = (props: any) => {
     const classes = styleCardActivity()
@@ -20,22 +21,22 @@ export const CardActivity = (props: any) => {
             <Paper elevation={3} className={classes.card}>
                 <Grid container >
                     <Grid item xs={1} style={{textAlign: "center"}}>
-                        <CheckCircleOutlineIcon style={{ color: green[500] }}/>
+                        <CheckCircleOutlineIcon style = {testResults.testPassed ? { color: green[500] } : { color: red[500] } }/>
                     </Grid>
                     <Grid item xs={8}>
                         <Typography align={"left"} >
-                            Не прошел тест JavaScript основы
+                            Не прошел тест  <strong>{testResults.testId.name}</strong>
                         </Typography>
                     </Grid>
                     <Grid item xs={3}>
                         <Typography align={"right"}  color="textSecondary">
-                            14:51 19 August 2020
+                            {moment(testResults.datePassed).format('HH:mm LL')}
                         </Typography>
                     </Grid>
                 </Grid>
                 <div>
                     <Typography align={"left"} >
-                        прохождение #y0n73w с результатом: 6 из 15 правильных ответов и 24 очков рейтинга
+                        прохождение <Link to={`/rt/${testResults.uuid}`}>#{testResults.uuid.substring(0, 5)}</Link> с результатом:  {testResults.rightAnswer} из {testResults.summaryAnswer} правильных ответов
                     </Typography>
                 </div>
             </Paper>
